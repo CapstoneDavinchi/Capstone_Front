5dbbfdadc9652ec3e5c056c94032343f22bebbaf
최유빈 <youbeen@choeyubin-ui-MacBookPro.local>
2024-04-16 02:44:38 +0900

    Feat/초기 프로젝트 설정

---
+++ DavinciApp/DavinciApp/DavinciAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DavinciAppApp.swift
+//  DavinciApp
+//
+//  Created by 최유빈 on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DavinciAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DavinciApp/Pods/Target Support Files/Pods-DavinciApp/Pods-DavinciApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DavinciApp : NSObject
+@end
+@implementation PodsDummy_Pods_DavinciApp
+@end
