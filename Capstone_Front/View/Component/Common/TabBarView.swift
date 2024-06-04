456000b12ca3cdd2cc484ef09f5d405117f0d1e2
최유빈 <cub8533@naver.com>
2024-06-04 16:10:53 +0900

    Fix/TabView 정렬 수정

--- Capstone_Front/View/Component/Common/TabBarView.swift
+++ Capstone_Front/View/Component/Common/TabBarView.swift
@@ -12,27 +12,30 @@ struct TabBarView: View {
     
     var body: some View {
         HStack {
-            // 왼쪽 개인작품전시페이지
-            if viewModel.showPersonalPage {
-                            // PersonalPage가 활성화 되어 있을 때 뒤로 가기 버튼
-                Button(action: {
-                    viewModel.showPersonalPage = false
-                }) {
-                    Image(systemName: "chevron.left")
-                        .resizable()
-                        .frame(width: 7, height: 13)
-                        .foregroundColor(.white)
-                }
-            } else {
-                // PersonalPage가 비활성화 되어 있을 때 프로필 버튼
-                Button(action: {
-                    viewModel.showPersonalPage = true
-                }) {
-                    Image("Profile")
-                        .resizable()
-                        .frame(width: 32, height: 32)
+            ZStack {
+                // 왼쪽 개인작품전시페이지
+                if viewModel.showPersonalPage {
+                    // PersonalPage가 활성화 되어 있을 때 뒤로 가기 버튼
+                    Button(action: {
+                        viewModel.showPersonalPage = false
+                    }) {
+                        Image(systemName: "chevron.left")
+                            .resizable()
+                            .frame(width: 7, height: 13)
+                            .foregroundColor(.white)
+                    }
+                } else {
+                    // PersonalPage가 비활성화 되어 있을 때 프로필 버튼
+                    Button(action: {
+                        viewModel.showPersonalPage = true
+                    }) {
+                        Image("Profile")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                    }
                 }
             }
+            .frame(width: 32, height: 32)
             
             Spacer()
             
@@ -41,16 +44,19 @@ struct TabBarView: View {
                 .frame(width: 78, height: 12)
             Spacer()
             
-            Button(action: {
-                viewModel.showMenu.toggle() // 카테고리 메뉴 표시/ 표시 X
-            }) {
-                Image(systemName: "list.dash")
-                    .resizable()
-                    .frame(width: 22, height: 12)
+            ZStack {
+                Button(action: {
+                    viewModel.showMenu.toggle() // 카테고리 메뉴 표시/ 표시 X
+                }) {
+                    Image(systemName: "list.dash")
+                        .resizable()
+                        .frame(width: 22, height: 12)
+                }
             }
+            .frame(width: 32, height: 32)
         }
         .padding(.horizontal, 16)
-        .padding(.vertical, 18)
+        .frame(height: 64)
         .background(Color.clear)
         .foregroundColor(.white)
     }
--- Capstone_Front/ViewModel/TabBarViewModel.swift
+++ Capstone_Front/ViewModel/TabBarViewModel.swift
@@ -11,4 +11,5 @@ import SwiftUI
 class TabBarViewModel: ObservableObject {
     @Published var showMenu: Bool = false // 카테고리 메뉴 표시여부임.
     @Published var showPersonalPage = false // 개인작품전시페이지 이동 여부
+    @Published var showUploadPage = false // 업로드 페이지 이동 여부
 }
